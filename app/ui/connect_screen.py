e5a16a647e290e43512613fc02707dbc6f296ed0
José Felipe dos Santos Freitas <jose.freitas@fitbank.com.br>
2025-06-04 18:01:35 -0300

    continua em casa

--- app/ui/connect_screen.py
+++ app/ui/connect_screen.py
@@ -5,6 +5,7 @@ from app.utils import DatabaseConnectionManager as dcm, SavedConnectionsManager 
 
 class ConnectScreen:
     def __init__(self, master, position: dict, on_connect_callback=None):
+        # define gerenciadores de janelas e callbacks
         self.on_connect_callback = on_connect_callback
         self.was_cancelled = True
         # Criação da janela toplevel
@@ -15,7 +16,6 @@ class ConnectScreen:
         self.connect_window.title("Select connection")
         self.connect_window.grab_set()  # Garante que a janela de conexão seja modal
         self.connect_window.transient(master)  # Define a janela como filha da janela principal
-        self.connect_window.protocol("WM_DELETE_WINDOW", self._on_window_close)
 
         # Treeview para histórico de conexões
         frame_treeview = tk.Frame(self.connect_window, width=390, height=274)
@@ -34,22 +34,20 @@ class ConnectScreen:
 
         # cria menu de contexto
         self.menu = tk.Menu(self.connect_window, tearoff=0)
+
         self.menu.add_command(label="Refresh", command=self.get_saved_connections)
         self.menu.add_separator()
         self.menu.add_command(label="Connect", command=self.menu_connect)
-        self.menu.add_command(label="Delete", command=self.delete_selected_connection)
+        self.menu.add_command(label="Delete", command=self._delete_selected_connection)
         self.menu.add_separator()
         self.menu.add_command(label="Clear historic connection", command= self.clear_historic_connections)
         self.menu.add_command(label="Exit", command=self._on_window_close)
-        self.treeview.bind("<Button-3>", self.show_context_menu)
-
-        self.treeview.bind("<ButtonRelease-1>", self.on_treeview_select)
 
         # Menu de entrada
         frame_entry_menu = tk.Frame(self.connect_window, width=390, height=236, bg="#F0F0F0")
         frame_entry_menu.place(x=0, y=264)
 
-        # Server name
+        # nome do servidor
         frame_server_name = tk.Frame(frame_entry_menu, width=390, height=21, bg="#F0F0F0")
         frame_server_name.place(x=0, y=21)
         label_server_name = tk.Label(frame_server_name, text="Server name:", font=("Inter", 10), bg="#F0F0F0", fg="#000000")
@@ -63,11 +61,13 @@ class ConnectScreen:
         label_authentication = tk.Label(frame_authentication, text="Authentication:", font=("Inter", 10), bg="#F0F0F0", fg="#000000")
         label_authentication.place(x=10, y=0)
 
-        # Substitui o entry_authentication por um dropdown
+        # tipo de autenticação
         self.dropdown_authentication = ttk.Combobox(frame_authentication, width=27, state="readonly")
         self.dropdown_authentication.place(x=120, y=0)
         self.dropdown_authentication['values'] = ("Windows Authentication", "SQL Authentication")
         self.dropdown_authentication.current(0)  # Define como padrão "Windows Authentication"
+
+        # Atualiza os campos de autenticação quando o tipo é alterado
         def update_authentication(event):
             if self.dropdown_authentication.get() == "Windows Authentication":
                 self.entry_user_name.config(state="normal")
@@ -82,7 +82,7 @@ class ConnectScreen:
 
         self.dropdown_authentication.bind("<<ComboboxSelected>>", update_authentication)
 
-        # User name
+        # nome do usuário
         frame_user_name = tk.Frame(frame_entry_menu, width=390, height=21, bg="#F0F0F0")
         frame_user_name.place(x=0, y=84)
         label_user_name = tk.Label(frame_user_name, text="User name:", font=("Inter", 10), bg="#F0F0F0", fg="#000000")
@@ -90,7 +90,7 @@ class ConnectScreen:
         self.entry_user_name = tk.Entry(frame_user_name, width=30, bg="#FFFEFE")
         self.entry_user_name.place(x=120, y=0)
 
-        # Password
+        # senha
         frame_password = tk.Frame(frame_entry_menu, width=390, height=21, bg="#F0F0F0")
         frame_password.place(x=0, y=116)
         label_password = tk.Label(frame_password, text="Password:", font=("Inter", 10), bg="#F0F0F0", fg="#000000")
@@ -98,17 +98,13 @@ class ConnectScreen:
         self.entry_password = tk.Entry(frame_password, width=30, bg="#FFFEFE", show="*")
         self.entry_password.place(x=120, y=0)
 
-        # Database Name
+        # nome do banco de dados
         frame_database_name = tk.Frame(frame_entry_menu, width=390, height=21, bg="#F0F0F0")
         frame_database_name.place(x=0, y=146)
         label_database_name = tk.Label(frame_database_name, text="Database Name:", font=("Inter", 10), bg="#F0F0F0", fg="#000000")
         label_database_name.place(x=10, y=0)
-
-        # Cria um combobox para seleção de opções
         self.dropdown_database_name = ttk.Combobox(frame_database_name, width=27)
         self.dropdown_database_name.place(x=120, y=0)
-
-        self.dropdown_database_name.bind("<Button-1>", lambda e: threading.Thread(target=self.get_databases).start())
 
         # Check Remember
         frame_check_remember = tk.Frame(frame_entry_menu, width=100, height=21, bg="#F0F0F0")
@@ -128,7 +124,18 @@ class ConnectScreen:
         update_authentication(None)
         self.get_saved_connections()
 
-    def delete_selected_connection(self):
+        # Bindings e eventos
+        self.connect_window.bind("<Return>", lambda event: self.on_connect_btn())
+        self.connect_window.protocol("WM_DELETE_WINDOW", self._on_window_close)
+        self.treeview.bind("<Button-3>", self.show_context_menu)
+        self.treeview.bind("<ButtonRelease-1>", self.on_treeview_select)
+        self.treeview.bind("<double-Button-1>", self.menu_connect)
+        self.dropdown_database_name.bind("<Button-1>", lambda e: threading.Thread(target=self.get_databases).start())
+
+    def _delete_selected_connection(self):
+        """ Exclui a conexão selecionada no Treeview.
+        Se nenhuma conexão estiver selecionada, exibe um aviso."""
+        
         selected_items = self.treeview.selection()
         if not selected_items:
             messagebox.showwarning("Warning", "No connection selected to delete.")
